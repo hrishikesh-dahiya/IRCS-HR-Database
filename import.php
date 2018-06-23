9ac272e8cb53bb303c5bb93c9ce258339f27932c
hrishikesh-dahiya <f20160057@goa.bits-pilani.ac.in>
2018-06-23 22:46:49 +0530

    Add import data feature

--- import.php
+++ import.php
@@ -14,30 +14,50 @@ if(!$conn) {
     echo "Connection successful!<br>";
 }
 
+$tmp_name = $_FILES["upload-file"]["tmp_name"];
 
-$tmp_name = $_FILES["officiating-upload"]["tmp_name"];
-
-$name = basename($_FILES["officiating-upload"]["name"]);
+$name = basename($_FILES["upload-file"]["name"]);
 
 move_uploaded_file($tmp_name, "./tmp/".$name);
 
 $target_dir = "./tmp/";
 
-$target_file = $target_dir.basename($_FILES["officiating-upload"]["name"]);
+$target_file = $target_dir.basename($_FILES["upload-file"]["name"]);
 
+$which_table = $_POST["upload-file-btn"];
 
-if (($handle = fopen($target_file, "r")) !== FALSE) {
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-        $insert_query = "insert into officiating_position values(\"".$data[0]."\",\"".$data[1]."\",\"".date("Y-m-d", strtotime($data[2]))."\",\"".date("Y-m-d", strtotime($data[3]))."\")";
-
-        if(mysqli_query($conn, $insert_query)) {
-            echo "Query executed<br>"; 
-        } else {
-            echo "Query unsuccessful<br>";
+switch($which_table) {
+    case "officiating-upload":
+        if (($handle = fopen($target_file, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $insert_query = "insert into officiating_position values(\"".$data[0]."\",\"".$data[1]."\",\"".date("Y-m-d", strtotime($data[2]))."\",\"".date("Y-m-d", strtotime($data[3]))."\")";
+        
+                if(mysqli_query($conn, $insert_query)) {
+                    echo "Query executed<br>"; 
+                } else {
+                    echo "Query unsuccessful<br>";
+                }
+            }
+            fclose($handle);
         }
-    }
-    fclose($handle);
+        break;
+    default:
+        echo "Did not work<br>";
+        break;    
 }
+
+// if (($handle = fopen($target_file, "r")) !== FALSE) {
+//     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+//         $insert_query = "insert into officiating_position values(\"".$data[0]."\",\"".$data[1]."\",\"".date("Y-m-d", strtotime($data[2]))."\",\"".date("Y-m-d", strtotime($data[3]))."\")";
+
+//         if(mysqli_query($conn, $insert_query)) {
+//             echo "Query executed<br>"; 
+//         } else {
+//             echo "Query unsuccessful<br>";
+//         }
+//     }
+//     fclose($handle);
+// }
 
 unlink($target_file);
 
--- submit.php
+++ submit.php
@@ -28,12 +28,30 @@ switch($which_table) {
     case "officiating-fill":
         $insert_query = "insert into officiating_position values(\"".$_POST["employee_code"]."\",\"".$_POST["officiating_pos"]."\",\"".date("Y-m-d", strtotime($_POST["start_date"]))."\",\"".date("Y-m-d", strtotime($_POST["end_date"]))."\")";
         break;
+
     case "experience-fill":
         if($_POST["total"] == NULL){
             $_POST["total"] = (int)$_POST["with_ircs"] + (int)$_POST["outside_ircs"];
         }
         $insert_query = "insert into experience values (\"".$_POST["employee_code"]."\",\"".$_POST["with_ircs"]."\",\"".$_POST["outside_ircs"]."\",\"".$_POST["total"]."\")";
         break;
+
+    case "contract-fill":
+        $insert_query = "insert into contract values (\"".$_POST["employee_code"]."\",\"".$_POST["start_date"]."\",\"".$_POST["end_date"]."\",\"".$_POST["renewal"]."\")";
+        break;
+    
+    case "promotion-fill":
+        $insert_query = "insert into promotion values (\"".$_POST["employee_code"]."\",\"".$_POST["type-promotion"]."\",\"".$_POST["promoition-date"]."\",\"".$_POST["details-promotion"]."\")";
+        break;
+
+    case "salary-fill":
+        $insert_query = "insert into salary_allowances values (\"".$_POST["employee_code"]."\",\"".$_POST["band"]."\",\"".$_POST["level"]."\",\"".$_POST["band_pay"]."\",\"".$_POST["grade_pay"]."\",\"".$_POST["npa"]."\",\"".$_POST["sca"]."\",\"".$_POST["tda_arr"]."\",\"".$_POST["da"]."\",\"".$_POST["transport"]."\",\"".$_POST["tda"]."\",\"".$_POST["hra"]."\",\"".$_POST["cca"]."\",\"".$_POST["wash"]."\",\"".$_POST["hca"]."\",\"".$_POST["ewa"]."\",\"".$_POST["others"]."\",\"".$_POST["da_arr"]."\",\"".$_POST["ex_gratia"]."\",\"".$_POST["gross"]."\",\"".$_POST["pf"]."\",\"".$_POST["fg"]."\",\"".$_POST["monthly_cto"]."\",\"".$_POST["monthly_cto_fg"]."\",\"".$_POST["annual_cto"]."\",\"".$_POST["annual_cto_fg"]."\")";
+        break;
+
+    case "personal-fill":
+        $insert_query = "insert into salary_allowances values (\"".$_POST["employee_code"]."\",\"".$_POST["title"]."\",\"".$_POST["name"]."\",\"".$_POST["sex"]."\",\"".$_POST["dob"]."\",\"".$_POST["age"]."\",\"".$_POST["staff_classification"]."\",\"".$_POST["designation"]."\",\"".$_POST["reporting_manager"]."\",\"".$_POST["department"]."\",\"".$_POST["location"]."\",\"".$_POST["date_of_joining"]."\",\"".$_POST["diversity_status"]."\",\"".$_POST["educational_qualification"]."\",\"".$_POST["education_details"]."\",\"".$_POST["professional_qualification"]."\",\"".$_POST["present_address"]."\",\"".$_POST["permanent_address"]."\",\"".$_POST["city"]."\",\"".$_POST["state"]."\",\"".$_POST["phone"]."\",\"".$_POST["emergency_no"]."\",\"".$_POST["email"]."\",\"".$_POST["marital_status"]."\",\"".$_POST["father_husband_name"]."\",\"".$_POST["father_husband"]."\",\"".$_POST["nominee_name"]."\",\"".$_POST["relationship"]."\",\"".$_POST["bank_name"]."\",\"".$_POST["ifsc_code"]."\",\"".$_POST["acc_no"]."\",\"".$_POST["aadhar_no"]."\",\"".$_POST["pan_no"]."\",\"".$_POST["passport_no"]."\",\"".$_POST["pf_no"]."\",\"".$_POST["uan_no"]."\",\"".$_POST["last_working_day"]."\",\"".$_POST["status"]."\")";
+        break;
+
     default:
         echo "Did not work <br>";
         break;
