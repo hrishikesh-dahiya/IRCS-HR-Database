77e4b67408fa4950611200a6a9bd226227b05d05
hrishikesh-dahiya <f20160057@goa.bits-pilani.ac.in>
2018-06-24 10:00:59 +0530

    Complete prev features

--- import.php
+++ import.php
@@ -41,23 +41,89 @@ switch($which_table) {
             fclose($handle);
         }
         break;
+
+    case "contract-upload":
+        if (($handle = fopen($target_file, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $insert_query = "insert into contract values (\"".$data[0]."\",\"".date("Y-m-d", strtotime($data[1]))."\",\"".date("Y-m-d", strtotime($data[2]))."\",\"".date("Y-m-d", strtotime($data[3]))."\")";
+
+                if(mysqli_query($conn, $insert_query)) {
+                    echo "Query executed<br>"; 
+                } else {
+                    echo "Query unsuccessful<br>";
+                }
+            }
+            fclose($handle);
+        }
+        break;
+
+    case "experience-upload":
+        if (($handle = fopen($target_file, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                if($data[3] == NULL){
+                    $data[3] = (int)$data[1] + (int)$data[2];
+                }
+                $insert_query = "insert into experience values (\"".$data[0]."\",\"".$data[1]."\",\"".$data[2]."\",\"".$data[3]."\")";
+                
+                if(mysqli_query($conn, $insert_query)) {
+                    echo "Query executed<br>"; 
+                } else {
+                    echo "Query unsuccessful<br>";
+                }
+            }
+            fclose($handle);
+        }
+        break;
+
+    case "promotion-upload":
+        if (($handle = fopen($target_file, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $insert_query = "insert into promotion values (\"".$data[0]."\",\"".$data[1]."\",\"".date("Y-m-d", strtotime($data[2]))."\",\"".$data[3]."\")";
+        
+                if(mysqli_query($conn, $insert_query)) {
+                    echo "Query executed<br>"; 
+                } else {
+                    echo "Query unsuccessful<br>";
+                }
+            }
+            fclose($handle);
+        }
+        break;
+
+    case "salary-upload":
+        if (($handle = fopen($target_file, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $insert_query = "insert into salary_allowances values (\"".$data[0]."\",\"".$data[1]."\",\"".$data[2]."\",\"".$data[3]."\",\"".$data[4]."\",\"".$data[5]."\",\"".$data[6]."\",\"".$data[7]."\",\"".$data[8]."\",\"".$data[9]."\",\"".$data[10]."\",\"".$data[11]."\",\"".$data[12]."\",\"".$data[13]."\",\"".$data[14]."\",\"".$data[15]."\",\"".$data[16]."\",\"".$data[17]."\",\"".$data[18]."\",\"".$data[19]."\",\"".$data[20]."\",\"".$data[21]."\",\"".$data[22]."\",\"".$data[23]."\",\"".$data[24]."\",\"".$data[25]."\")";
+        
+                if(mysqli_query($conn, $insert_query)) {
+                    echo "Query executed<br>"; 
+                } else {
+                    echo "Query unsuccessful<br>";
+                }
+            }
+            fclose($handle);
+        }
+        break;
+
+    case "personal-upload":
+        if (($handle = fopen($target_file, "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $insert_query = "insert into personal_information values (\"".$data[0]."\",\"".$data[1]."\",\"".$data[2]."\",\"".$data[3]."\",\"".date("Y-m-d", strtotime($data[4]))."\",\"".$data[5]."\",\"".$data[6]."\",\"".$data[7]."\",\"".$data[8]."\",\"".$data[9]."\",\"".$data[10]."\",\"".date("Y-m-d", strtotime($data[11]))."\",\"".$data[12]."\",\"".$data[13]."\",\"".$data[14]."\",\"".$data[15]."\",\"".$data[16]."\",\"".$data[17]."\",\"".$data[18]."\",\"".$data[19]."\",\"".$data[20]."\",\"".$data[21]."\",\"".$data[22]."\",\"".$data[23]."\",\"".$data[24]."\",\"".$data[25]."\",\"".$data[26]."\",\"".$data[27]."\",\"".$data[28]."\",\"".$data[29]."\",\"".$data[30]."\",\"".$data[31]."\",\"".$data[32]."\",\"".$data[33]."\",\"".$data[34]."\",\"".$data[35]."\",\"".date("Y-m-d", strtotime($data[36]))."\",\"".$data[37]."\")";
+        
+                if(mysqli_query($conn, $insert_query)) {
+                    echo "Query executed<br>"; 
+                } else {
+                    echo "Query unsuccessful<br>";
+                }
+            }
+            fclose($handle);
+        }
+        break;
+        
     default:
         echo "Did not work<br>";
         break;    
 }
-
-// if (($handle = fopen($target_file, "r")) !== FALSE) {
-//     while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-//         $insert_query = "insert into officiating_position values(\"".$data[0]."\",\"".$data[1]."\",\"".date("Y-m-d", strtotime($data[2]))."\",\"".date("Y-m-d", strtotime($data[3]))."\")";
-
-//         if(mysqli_query($conn, $insert_query)) {
-//             echo "Query executed<br>"; 
-//         } else {
-//             echo "Query unsuccessful<br>";
-//         }
-//     }
-//     fclose($handle);
-// }
 
 unlink($target_file);
 
--- submit.php
+++ submit.php
@@ -37,11 +37,11 @@ switch($which_table) {
         break;
 
     case "contract-fill":
-        $insert_query = "insert into contract values (\"".$_POST["employee_code"]."\",\"".$_POST["start_date"]."\",\"".$_POST["end_date"]."\",\"".$_POST["renewal"]."\")";
+        $insert_query = "insert into contract values (\"".$_POST["employee_code"]."\",\"".date("Y-m-d", strtotime($_POST["start_date"]))."\",\"".date("Y-m-d", strtotime($_POST["end_date"]))."\",\"".date("Y-m-d", strtotime($_POST["renewal"]))."\")";
         break;
     
     case "promotion-fill":
-        $insert_query = "insert into promotion values (\"".$_POST["employee_code"]."\",\"".$_POST["type-promotion"]."\",\"".$_POST["promoition-date"]."\",\"".$_POST["details-promotion"]."\")";
+        $insert_query = "insert into promotion values (\"".$_POST["employee_code"]."\",\"".$_POST["type-promotion"]."\",\"".date("Y-m-d", strtotime($_POST["promotion-date"]))."\",\"".$_POST["details-promotion"]."\")";
         break;
 
     case "salary-fill":
@@ -49,7 +49,7 @@ switch($which_table) {
         break;
 
     case "personal-fill":
-        $insert_query = "insert into salary_allowances values (\"".$_POST["employee_code"]."\",\"".$_POST["title"]."\",\"".$_POST["name"]."\",\"".$_POST["sex"]."\",\"".$_POST["dob"]."\",\"".$_POST["age"]."\",\"".$_POST["staff_classification"]."\",\"".$_POST["designation"]."\",\"".$_POST["reporting_manager"]."\",\"".$_POST["department"]."\",\"".$_POST["location"]."\",\"".$_POST["date_of_joining"]."\",\"".$_POST["diversity_status"]."\",\"".$_POST["educational_qualification"]."\",\"".$_POST["education_details"]."\",\"".$_POST["professional_qualification"]."\",\"".$_POST["present_address"]."\",\"".$_POST["permanent_address"]."\",\"".$_POST["city"]."\",\"".$_POST["state"]."\",\"".$_POST["phone"]."\",\"".$_POST["emergency_no"]."\",\"".$_POST["email"]."\",\"".$_POST["marital_status"]."\",\"".$_POST["father_husband_name"]."\",\"".$_POST["father_husband"]."\",\"".$_POST["nominee_name"]."\",\"".$_POST["relationship"]."\",\"".$_POST["bank_name"]."\",\"".$_POST["ifsc_code"]."\",\"".$_POST["acc_no"]."\",\"".$_POST["aadhar_no"]."\",\"".$_POST["pan_no"]."\",\"".$_POST["passport_no"]."\",\"".$_POST["pf_no"]."\",\"".$_POST["uan_no"]."\",\"".$_POST["last_working_day"]."\",\"".$_POST["status"]."\")";
+        $insert_query = "insert into personal_information values (\"".$_POST["employee_code"]."\",\"".$_POST["title"]."\",\"".$_POST["name"]."\",\"".$_POST["sex"]."\",\"".date("Y-m-d", strtotime($_POST["dob"]))."\",\"".$_POST["age"]."\",\"".$_POST["staff_classification"]."\",\"".$_POST["designation"]."\",\"".$_POST["reporting_manager"]."\",\"".$_POST["department"]."\",\"".$_POST["location"]."\",\"".date("Y-m-d", strtotime($_POST["date_of_joining"]))."\",\"".$_POST["diversity_status"]."\",\"".$_POST["educational_qualification"]."\",\"".$_POST["education_details"]."\",\"".$_POST["professional_qualification"]."\",\"".$_POST["present_address"]."\",\"".$_POST["permanent_address"]."\",\"".$_POST["city"]."\",\"".$_POST["state"]."\",\"".$_POST["phone"]."\",\"".$_POST["emergency_no"]."\",\"".$_POST["email"]."\",\"".$_POST["marital_status"]."\",\"".$_POST["father_husband_name"]."\",\"".$_POST["father_husband"]."\",\"".$_POST["nominee_name"]."\",\"".$_POST["relationship"]."\",\"".$_POST["bank_name"]."\",\"".$_POST["ifsc_code"]."\",\"".$_POST["acc_no"]."\",\"".$_POST["aadhar_no"]."\",\"".$_POST["pan_no"]."\",\"".$_POST["passport_no"]."\",\"".$_POST["pf_no"]."\",\"".$_POST["uan_no"]."\",\"".date("Y-m-d", strtotime($_POST["last_working_day"]))."\",\"".$_POST["status"]."\")";
         break;
 
     default:
